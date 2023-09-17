3f215e1d86d8d58bd7ab4bb200a333aa96a0096b
Chonlasak Tumna <137064083+ChonlasakThamana@users.noreply.github.com>
2023-09-17 14:20:34 +0700

    9-5

--- Homework/HW09/hw09-5-display-only-unique-number.cpp
+++ Homework/HW09/hw09-5-display-only-unique-number.cpp
@@ -36,4 +36,43 @@
     Output:
         Unique value : 1 2 5 6
 
-*/
+*/#include <stdio.h>
+
+int main() {
+    int N;
+
+    // รับค่า N จากผู้ใช้
+    printf("Input N: ");
+    scanf("%d", &N);
+
+    // สร้างอาเรย์เพื่อเก็บข้อมูล
+    int arr[N];
+
+    // รับข้อมูลจากผู้ใช้และเพิ่มลงในอาเรย์
+    for (int i = 0; i < N; i++) {
+        printf("Input: ");
+        scanf("%d", &arr[i]);
+    }
+
+    // ตรวจสอบและแสดงค่าที่ไม่ซ้ำกัน
+    printf("Unique value: ");
+
+    for (int i = 0; i < N; i++) {
+        int isUnique = 1;
+
+        // ตรวจสอบว่าค่านี้มีการซ้ำกันหรือไม่
+        for (int j = 0; j < N; j++) {
+            if (i != j && arr[i] == arr[j]) {
+                isUnique = 0;
+                break;
+            }
+        }
+
+        // ถ้าไม่มีการซ้ำกันให้แสดงค่า
+        if (isUnique) {
+            printf("%d ", arr[i]);
+        }
+    }
+
+    return 0;
+}
